d86778bddaf88ba05d3cab74c6d60af1cd3c2124
Øystein Hofvind-Rom <oystein@hofvind-rom.com>
2020-01-09 18:26:10 +0100

    - Minor code changes - Added comments - Added readme.md

--- main.py
+++ main.py
@@ -5,25 +5,36 @@ import datetime
 import pathlib
 
 
-async def time(websocket, path):
+# Websocket server loop
+async def time(websocket, path):    # ws_handler requires 2 arguments, path not used in this code
     while True:
         try:
-            now = datetime.datetime.now().time().isoformat()
+            # Get current time, isoformat() makes it a string in ISO 8601 format
+            now = datetime.datetime.now().time().isoformat(timespec='seconds')
 
-            await websocket.send(now)
-            await asyncio.sleep(1)
+            await websocket.send(now)       # Send current time to GUI
+            await asyncio.sleep(1)          # Wait one second before running loop again
+
+        # If GUI is closed, exit the while loop
         except websockets.ConnectionClosed:
             break
 
+    # Stop the asyncio event loop - closing the program
     asyncio.get_event_loop().stop()
 
 
 def main():
-    gui_location = str(pathlib.Path().absolute())
+    # Assign the websocket server
     start_server = websockets.serve(time, "127.0.0.1", 5678)
+
+    # (mac os) Get current path and open the gui in default web browser
+    gui_location = str(pathlib.Path().absolute())
     webbrowser.open_new_tab("file://" + gui_location + "/gui/index.html")
 
+    # Start the websocket server
     asyncio.get_event_loop().run_until_complete(start_server)
+
+    # Keep the program running
     asyncio.get_event_loop().run_forever()
 
 
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Minimal websocket app
+### Runtime environment
+OS: Mac OS  
+Python version: 3.8.1  
+Browser: Chrome  
+
+### About this app
+I want to use HTML, CSS and JS as front-end for Python apps, so as a part of 
+learning it I made this mini project that updates the page without needing to 
+refresh the browser. It also detects when the page is closed and then exits the back-end app.  
+
+### Disclaimer
+This is only tested on mac os, and the path for the html code will probably not work on Windows.
+I am also a beginner when it comes to programming, so it is probably not optimal code ;)
